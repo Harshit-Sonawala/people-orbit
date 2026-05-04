a2c1d90b005ae6703309f883fa5f321c842b0a8c
Harshit-Sonawala <harshitsamirsonawala@gmail.com>
2026-05-04 13:04:19 +0530

    feat: Created logger middleware in common/logger.middleware.ts. Added logger to app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { PeopleModule } from './modules/people/people.module';
 
 @Module({
-  imports: [PeopleModule, ConfigModule.forRoot({isGlobal: true,})],
+  imports: [PeopleModule, ConfigModule.forRoot({ isGlobal: true, })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('people');
+  }
+}
