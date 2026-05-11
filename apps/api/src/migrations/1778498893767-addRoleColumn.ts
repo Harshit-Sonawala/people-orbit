55ce9ab6c1463d51de4a07f9a0cffe4730632eec
Harshit-Sonawala <harshitsamirsonawala@gmail.com>
2026-05-11 17:29:19 +0530

    feat: Added migrations to create the initial table and then alter table to add the role column.

--- apps/api/src/migrations/1778498893767-addRoleColumn.ts
+++ apps/api/src/migrations/1778498893767-addRoleColumn.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddRoleColumn1778495554301 implements MigrationInterface {
+export class AddRoleColumn1778498893767 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "role_enum" AS ENUM('user', 'manager', 'admin')`);
