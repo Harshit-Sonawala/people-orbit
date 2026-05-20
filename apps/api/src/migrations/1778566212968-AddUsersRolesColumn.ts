1b83be70de2c6a327258789f2f16f58611097eb2
Harshit-Sonawala <harshitsamirsonawala@gmail.com>
2026-05-20 18:03:08 +0530

    feat: Created the AddUsersPasswordColumn migration. Auto formatter updated AddUsersRolesColumn migration. Ran successfully

--- apps/api/src/migrations/1778566212968-AddUsersRolesColumn.ts
+++ apps/api/src/migrations/1778566212968-AddUsersRolesColumn.ts
@@ -1,16 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddUsersRolesColumn1778566212968 implements MigrationInterface {
-
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TYPE "role_enum" AS ENUM('user', 'manager', 'admin')`);
-    await queryRunner.query(`ALTER TABLE "users" ADD "role" "role_enum" NOT NULL DEFAULT 'user'`);
+    await queryRunner.query(
+      `CREATE TYPE "role_enum" AS ENUM('user', 'manager', 'admin')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "role" "role_enum" NOT NULL DEFAULT 'user'`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "role"`);
     await queryRunner.query(`DROP TYPE "role_enum"`);
   }
-
-
 }
---
+++ apps/api/src/migrations/1779278084930-AddUsersPasswordColumn.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddUsersPasswordColumn1779278084930 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD "password" varchar NOT NULL DEFAULT 'PLACEHOLDER_PASS_HASH'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "password" DROP DEFAULT`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "password"`);
+  }
+}
